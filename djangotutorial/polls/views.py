b2c213d1dbf275314d74c0b276f73c94326b7dd0
alybt <alelybitun.school@gmail.com>
2026-02-02 10:26:53 +0800

    fix: polls/view.py

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -48,3 +48,7 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, "polls/detail.html", {"question": question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
