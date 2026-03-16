8a2e7147e5a289693a471aa2265cfcd1dd056b2c
alybt <alelybitun.school@gmail.com>
2026-03-16 10:06:47 +0800

    Add: Midterm

--- mid-term-exam/titanic_project/settings.py
+++ mid-term-exam/titanic_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'titanic_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'titanic_db',
+        'USER': 'MySQLServer',
+        'PASSWORD': '56585452',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
